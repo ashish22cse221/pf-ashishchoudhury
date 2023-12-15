dfe40cda5e77628fbd324f52762587b0216404f8
ASHISH CHOUDHURY <151498222+ashish22cse221@users.noreply.github.com>
2023-12-15 17:38:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["Front-end Developer","Web Developer","C-coder","Python-coder"],
+    typeSpeed:100,
+    backSpeed:90,
+    backDelay:1000,
+    loop:true
+})
